e1519aa4950f087720498314451f01962d2ad9b7
NickymanZX <114429569+NickymanZX@users.noreply.github.com>
2022-10-18 21:21:38 +0200

    Add files via upload

---
+++ Practica 18-10/main.js
@@ -0,0 +1,9 @@
+alert('¡Empezamos con javascript!')
+        console.log('Hola, soy un estudiante Hackademy')
+
+        let nombre = prompt('¿Cuál es tu nombre?');
+        let apellido = prompt('¿Cuál es tu apellido?');
+
+        let Bienvenido = 'Bienvenido '+ nombre + ' ' + apellido + ' ¿Quieres jugar a un juego?'
+
+        alert(Bienvenido);
